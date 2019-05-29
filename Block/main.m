a58a32b71a307b3aa4911056043d138111aa038e
liumingzhi <liumingzhi912@163.com>
2019-05-29 19:53:54 +0800

    Initial Commit

---
+++ Block/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Block
+//
+//  Created by mingzhi.liu on 2019/5/29.
+//  Copyright © 2019 mingzhi.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

fb6e70e742c39d3d58ca60c9a3df307c253317ba
liumingzhi003 <liumingzhi003@ke.com>
2019-08-02 17:24:22 +0800

    first commit

---
+++ Block/MJPerson.h
@@ -0,0 +1,17 @@
+//
+//  MJPerson.h
+//  Block
+//
+//  Created by mingzhi.liu on 2019/7/17.
+//  Copyright © 2019 mingzhi.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Block/MJPerson.m
@@ -0,0 +1,13 @@
+//
+//  MJPerson.m
+//  Block
+//
+//  Created by mingzhi.liu on 2019/7/17.
+//  Copyright © 2019 mingzhi.liu. All rights reserved.
+//
+
+#import "MJPerson.h"
+
+@implementation MJPerson
+
+@end

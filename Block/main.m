7cee07c5c034ce7b5e1f61b2145024256dd8f6c1
liumingzhi003 <liumingzhi003@ke.com>
2020-03-01 21:17:41 +0800

    提交 2

--- Block/main.m
+++ Block/main.m
@@ -13,6 +13,7 @@ int main(int argc, const char * argv[]) {
         // insert code here...
         NSLog(@"Hello, World!");
         NSLog(@"测试提交 1");
+        NSLog(@"测试提交 2");
 
     }
     return 0;
